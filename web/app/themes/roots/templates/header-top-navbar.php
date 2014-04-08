5040a8b79068f28fce434dc5d3ae094bcf79b422
Matthew Robitaille <matt@anchour.com>
2014-04-08 16:05:48 -0400

    Mobile fixes.

--- web/app/themes/roots/templates/header-top-navbar.php
+++ web/app/themes/roots/templates/header-top-navbar.php
@@ -10,7 +10,7 @@
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"></a>
     </div>
 
-    <nav role="navigation">
+    <nav class="collapse navbar-collapse" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu([
